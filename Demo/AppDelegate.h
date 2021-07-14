633aeb47894319db94de3129e407eeebce79342a
Shuhan Dong <dongshuhan.swan@bytedace.com>
2021-07-14 10:41:06 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by bytedance on 2021/7/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by bytedance on 2021/7/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
